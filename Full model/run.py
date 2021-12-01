1f7f8439e849ed878d0ea6ed9b74c022581351a7
maverheijen <91613757+maverheijen@users.noreply.github.com>
2021-12-01 14:23:29 +0100

    Running analyses

--- Full model/run.py
+++ Full model/run.py
@@ -15,18 +15,26 @@ import numpy as np
 
 #%%
 
+glu_to_ac = MDF_Analysis('glu_to_acetate_ratios_new')
+result = glu_to_ac.mdf_fixed_conc()
+result.plot_results()
+result = glu_to_ac.mdf_fixed_conc(fixed_rNADH=False)
+result.plot_results()
+
+#%%
+
 glu_to_ac_ebif = MDF_Analysis('glu_to_ac_e-bfc_ratios_new')
-
-#glu_to_ac_ebif.set_solver_tol(1e-7)
 result_ebif = glu_to_ac_ebif.mdf_fixed_conc()
-
 result_ebif.plot_results()
+result_ebif = glu_to_ac_ebif.mdf_fixed_conc(fixed_rNADH=False)
+result_ebif.plot_results()
+
 
 #%% Vary pi pool and NADH ratio
 varPi = [10e-3, 20e-3, 30e-3]
 var_rNADH = [5e-4, 5e-2, 0.5]
 
-glu_to_ac_ebif.set_solver_tol(1e-7)
+#glu_to_ac_ebif.set_solver_tol(1e-7)
 
 conc = []
 conc_ebif = []
@@ -35,16 +43,9 @@ dg_opt = []
 dg_opt_ebif = []
 
 for valPi in varPi:
-    glu_to_ac.set_maxPi(valPi)
     glu_to_ac_ebif.set_maxPi(valPi)
     for rNADH in var_rNADH:
-        glu_to_ac.set_rNADH(rNADH)
         glu_to_ac_ebif.set_rNADH(rNADH)
-        
-        result = glu_to_ac.mdf_fixed_conc()
-        conc_val, dg_val = result.results_table()
-        conc += [{'maxPi' : valPi, 'rNADH': rNADH, 'conc': conc_val}]
-        dg_opt += [{'maxPi' : valPi, 'rNADH': rNADH, 'dg': dg_val}]
         
         result_ebif = glu_to_ac_ebif.mdf_fixed_conc()
         conc_val, dg_val = result_ebif.results_table()
@@ -54,18 +55,18 @@ for valPi in varPi:
 #%%
 #make figure
 plt.figure(figsize=(13,9))
-plt.suptitle(result._netreaction_eq)
+plt.suptitle(result_ebif._netreaction_eq)
 
 #inidividual deltaG values of reactions
 plt.title('Optimized reaction energies')
 
-for i in range(0,len(conc)):
-    plot_conc = list(conc[i]['conc'].values())
+for i in range(0,len(conc_ebif)):
+    plot_conc = list(conc_ebif[i]['conc'].values())
     plot_dg = []
-    for val in list(dg_opt[i]['dg'].values()):
+    for val in list(dg_opt_ebif[i]['dg'].values()):
         plot_dg += [float(val)]
     
-    plt.plot(result._reactions, plot_dg, 'o', label = f"rNADH = {conc[i]['rNADH']}, maxPi = {conc[i]['maxPi']}")
+    plt.plot(result_ebif._reactions, plot_dg, 'o', label = f"rNADH = {conc_ebif[i]['rNADH']}, maxPi = {conc_ebif[i]['maxPi']}")
 
 plt.grid()
 plt.ylabel('$\Delta$G [kJ/mol]')
