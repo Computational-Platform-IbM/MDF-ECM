e2cb4f32ce03d386b7f2a89e0d1d23e8abf6546a
maverheijen <91613757+maverheijen@users.noreply.github.com>
2021-12-01 17:50:02 +0100

    Playing with rNADH in glu to ac no ebif

--- Full model/run.py
+++ Full model/run.py
@@ -16,60 +16,88 @@ import numpy as np
 #%%
 
 glu_to_ac = MDF_Analysis('glu_to_acetate_ratios_new')
-result = glu_to_ac.mdf_fixed_conc()
+
+result = glu_to_ac.execute_mdf_basis(set_fixed_c=True, fixed_rNADH = True, phys_bounds = True)
 result.plot_results()
-result = glu_to_ac.mdf_fixed_conc(fixed_rNADH=False)
+print(result._dg_prime_opt)
+print(sum(result._dg_prime_opt))
+print(result._rATP)
+
+result = glu_to_ac.execute_mdf_basis(set_fixed_c=True, fixed_rNADH = True, phys_bounds = False)
 result.plot_results()
+print(result._dg_prime_opt)
+print(sum(result._dg_prime_opt))
+print(result._rATP)
+
+result = glu_to_ac.execute_mdf_basis(set_fixed_c=True, fixed_rNADH = False, phys_bounds = True)
+result.plot_results()
+print(result._dg_prime_opt)
+print(sum(result._dg_prime_opt))
+print(result._rATP)
 
 #%%
 
-glu_to_ac_ebif = MDF_Analysis('glu_to_ac_e-bfc_ratios_new')
-result_ebif = glu_to_ac_ebif.mdf_fixed_conc()
-result_ebif.plot_results()
-result_ebif = glu_to_ac_ebif.mdf_fixed_conc(fixed_rNADH=False)
-result_ebif.plot_results()
 
 
-#%% Vary pi pool and NADH ratio
-varPi = [10e-3, 20e-3, 30e-3]
-var_rNADH = [5e-4, 5e-2, 0.5]
-
-#glu_to_ac_ebif.set_solver_tol(1e-7)
+#%% Vary NADH ratio
+var_rNADH = [3e-6, 5e-2, 0.16, 0.4, 1/240]
 
 conc = []
-conc_ebif = []
 
 dg_opt = []
-dg_opt_ebif = []
 
-for valPi in varPi:
-    glu_to_ac_ebif.set_maxPi(valPi)
-    for rNADH in var_rNADH:
-        glu_to_ac_ebif.set_rNADH(rNADH)
-        
-        result_ebif = glu_to_ac_ebif.mdf_fixed_conc()
-        conc_val, dg_val = result_ebif.results_table()
-        conc_ebif += [{'maxPi' : valPi, 'rNADH': rNADH, 'conc': conc_val}]
-        dg_opt_ebif += [{'maxPi' : valPi, 'rNADH': rNADH, 'dg': dg_val}]
+
+for rNADH in var_rNADH:
+    glu_to_ac.set_rNADH(rNADH)
+    
+    result = glu_to_ac.execute_mdf_basis(set_fixed_c=True, fixed_rNADH = True, phys_bounds = True)
+    conc_val, dg_val = result.results_table()
+    conc += [{'rNADH': rNADH, 'conc': conc_val}]
+    dg_opt += [{'rNADH': rNADH, 'dg': dg_val}]
 
 #%%
 #make figure
-plt.figure(figsize=(13,9))
-plt.suptitle(result_ebif._netreaction_eq)
+fig = plt.figure(figsize=(13,9))
+plt.suptitle(result._netreaction_eq)
+
+#inidividual deltaG values of reactions
+ax2 = fig.add_subplot(211)
+ax3 = fig.add_subplot(212)
 
 #inidividual deltaG values of reactions
 plt.title('Optimized reaction energies')
 
-for i in range(0,len(conc_ebif)):
-    plot_conc = list(conc_ebif[i]['conc'].values())
-    plot_dg = []
-    for val in list(dg_opt_ebif[i]['dg'].values()):
-        plot_dg += [float(val)]
-    
-    plt.plot(result_ebif._reactions, plot_dg, 'o', label = f"rNADH = {conc_ebif[i]['rNADH']}, maxPi = {conc_ebif[i]['maxPi']}")
+#concentrations
+comp_exceptions = ['H+', 'H2O', 'H2', 'rFd', 'rNADH', 'rNADPH', 'rATP']
+remove = []
+for i, comp in enumerate(result._compounds):
+    if comp in comp_exceptions:
+        remove += [i]
 
-plt.grid()
-plt.ylabel('$\Delta$G [kJ/mol]')
-plt.xlabel('Reactions')
-plt.ylim([-20, 10])
-plt.legend()
+plot_compounds = np.delete(np.array(result._compounds), remove)
+
+for i in range(0,len(conc)):
+    plot_conc = list(conc[i]['conc'].values())
+    plot_conc = [float(val) for val in plot_conc]
+    plot_conc = np.delete(np.array(plot_conc), remove)
+    
+    plot_dg = [float(val) for val in list(dg_opt[i]['dg'].values())]
+    
+    ax2.plot(result._reactions, plot_dg, 'o', label = f"rNADH = {conc[i]['rNADH']}")
+    
+    ax3.plot(plot_compounds, plot_conc, 'o--', label = f"rNADH = {conc[i]['rNADH']}")
+
+ax2.grid(True)
+ax3.grid(True)
+ax2.set_ylabel('$\Delta$G [kJ/mol]')
+ax2.set_xlabel('Reactions')
+#plt.ylim([-20, 0])
+ax2.legend()
+
+ax3.set_xticklabels(plot_compounds, fontsize=8, rotation=90)
+ax3.plot([0, len(plot_conc)], [10**-6, 10**-6], 'k-')
+ax3.plot([0, len(plot_conc)], [10**-2, 10**-2], 'k-')
+ax3.set_yscale('log')
+ax3.legend()
+
+plt.tight_layout()
